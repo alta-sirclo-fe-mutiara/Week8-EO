209cd5c3ad063a47884cc767b64aeda7e0b5ad9e
Mutiara A'yun <mutiara.ayun19@gmail.com>
2022-02-07 07:54:58 +0700

    Profile (#13)
    * Muti - data user
    
    * Muti - profil user

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -2,52 +2,52 @@ import React, { useEffect, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 type User = {
-  name: string;
-  email: string;
+	name: string;
+	email: string;
 };
 export interface AuthContextInterface {
-  user: User | null;
-  token: string;
-  isLogged: boolean;
+	user: User | null;
+	token: string;
+	isLogged: boolean;
 }
 
 const initialState = {
-  user: {
-    name: "",
-    email: "",
-  },
-  token: "",
-  isLogged: false,
+	user: {
+		name: "",
+		email: "",
+	},
+	token: "",
+	isLogged: false,
 };
 
 export const AuthContext = React.createContext<{
-  state: AuthContextInterface;
-  dispatch: React.Dispatch<any>;
+	state: AuthContextInterface;
+	dispatch: React.Dispatch<any>;
 }>({
-  state: initialState,
-  dispatch: () => null,
+	state: initialState,
+	dispatch: () => null,
 });
 
 export const AuthContextProvide = ({ children }: any) => {
-  const [state, dispatch] = useReducer(AuthReducer, initialState);
-  const isServer = typeof window === "undefined";
-  useEffect(() => {
-    if (!isServer) {
-      const users = localStorage.getItem("users") || "{}";
-      const user = JSON.parse(users);
-      if (user.token) {
-        dispatch({ type: "LOGIN_SUCCESS", payload: user });
-      }
-    }
-  }, [isServer]);
-  return (
-    <AuthContext.Provider
-      value={{
-        state,
-        dispatch,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+	const [state, dispatch] = useReducer(AuthReducer, initialState);
+	const isServer = typeof window === "undefined";
+	useEffect(() => {
+		if (!isServer) {
+			const users = localStorage.getItem("users") || "{}";
+			const user = JSON.parse(users);
+			if (user.token) {
+				dispatch({ type: "LOGIN_SUCCESS", payload: user });
+			}
+		}
+	}, [isServer]);
+	return (
+		<AuthContext.Provider
+			value={{
+				state,
+				dispatch,
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
