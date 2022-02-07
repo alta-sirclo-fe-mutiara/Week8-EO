672cb08a8d8e0433f8657f2a305f3fa1c1ca1755
Mutiara A'yun <mutiara.ayun19@gmail.com>
2022-02-07 08:12:57 +0700

    add comment (#14)
    * Muti - data user
    
    * add comment
    
    * Muti - profil user
    
    * Squashed commit of the following:
    
    commit 622c5e133546f1eea376f08a9db6072ab91937c9
    Author: Mutiara A'yun <mutiara.ayun19@gmail.com>
    Date:   Mon Feb 7 07:46:22 2022 +0700
    
        Muti - profil user
    
    commit 7a0e75c9e5e577721cea98c4f2240b5b8e7264fb
    Author: Mutiara A'yun <mutiara.ayun19@gmail.com>
    Date:   Sun Feb 6 17:08:56 2022 +0700
    
        Muti - data user
    
    Co-authored-by: lukmannm <luckymanlukman19@gmail.com>

--- components/UI/Comment/CommentList.tsx
+++ components/UI/Comment/CommentList.tsx
@@ -1,27 +1,25 @@
 import styles from "../../../styles/detail.module.css";
-const CommentList = () => {
-	return (
-		<div className="d-flex flex-row gap-4">
-			<div className="d-flex flex-row gap-4">
-				<div className="col-sm-1">
-					<img
-						src="https://www.markuptag.com/images/image-six.jpg"
-						alt="Picture of our Logo"
-						className={`${styles.img} rounded-circle`}
-					/>
-				</div>
-				<div className="ps-2">
-					<h6 className="fw-bold">Zhongli</h6>
-					<p className="text-secondary">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
-						obcaecati vel quod atque aliquam dolore odio. Fugit quisquam, neque
-						ducimus cum non ullam. Adipisci voluptatibus maiores corporis sed
-						necessitatibus cum.
-					</p>
-				</div>
-			</div>
-		</div>
-	);
+
+type CommentProps = {
+  comment: any;
+};
+const CommentList: React.FC<CommentProps> = ({ comment }) => {
+  const { comment: commen } = comment;
+  return (
+    <div className="d-flex flex-row gap-5">
+      <div className="col-sm-1">
+        <img
+          src="https://www.markuptag.com/images/image-six.jpg"
+          alt="Picture of our Logo"
+          className={`${styles.img} rounded-circle`}
+        />
+      </div>
+      <div className="">
+        <h6 className="fw-bold">Zhongli</h6>
+        <p className="text-secondary">{commen}</p>
+      </div>
+    </div>
+  );
 };
 
 export default CommentList;
