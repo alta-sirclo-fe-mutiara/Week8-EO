d031d9139013e0f6275c63b62c6dee54ba4cfa45
Mutiara A'yun <mutiara.ayun19@gmail.com>
2022-02-07 12:20:05 +0700

    fix comment

--- components/UI/Comment/CommentList.tsx
+++ components/UI/Comment/CommentList.tsx
@@ -7,15 +7,15 @@ const CommentList: React.FC<CommentProps> = ({ comment }) => {
 	const { comment: commen } = comment;
 	return (
 		<div className="d-flex flex-row gap-lg-5 gap-3 my-2">
-			<div className="col-sm-1">
+			<div className="col-sm-1 col-md-1 ">
 				<img
 					src="https://www.markuptag.com/images/image-six.jpg"
 					alt="Picture of our Logo"
 					className={`${styles.img} rounded-circle`}
 				/>
 			</div>
-			<div className="col-sm-10 ">
-				<h6 className="fw-bold mt-2">Zhongli</h6>
+			<div className="col-sm-10 col-md-8  ms-lg-0 ms-sm-5">
+				<h6 className="fw-bold mt-lg-2">Zhongli</h6>
 				<p className="text-secondary">{commen}</p>
 			</div>
 		</div>
